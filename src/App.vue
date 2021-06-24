f5d545cf219fe8dadf188e8feb09d8152bd302fe
LeoCaetano <leonardocaetano95@yahoo.com.br>
2021-06-24 18:33:16 -0300

    commit com a estrutura do projeto

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <jogador/>
+        </div>
+        <div class="col-3">
+          <jogador/>
+        </div>
+        <div class="col-3">
+          <jogador/>
+        </div>
+        <div class="col-3">
+          <jogador/>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <jogador/>
+        </div>
+        <div class="col-4">
+          <jogador/>
+        </div>
+        <div class="col-4">
+          <jogador/>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import Jogador from './components/Jogador.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Jogador
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Jogador.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="main">
+      <div class="container">
+          <div class="card">
+              <div class="card-body"> 
+                  <h1>{{Nome}} <span class="fa fa-check"></span></h1>
+                   <input type="text" v-model="Nome"> 
+              </div>
+
+          </div>
+
+      </div>
+
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css"
+import "font-awesome/css/font-awesome.css"
+export default {
+    name: "Jogador",
+    data: function(){
+        return {
+            Nome:"Teste"
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
